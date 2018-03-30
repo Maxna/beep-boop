0759bcaa75ff7be6f062a6489be87b9e4884b4f5
Matt Nardoni <nardoni59@gmail.com>
2018-03-30 13:57:09 -0700

    Add user functionality through branching)

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,30 @@
-function beepConverter(input) {
-  // var inputNumber = parseInt(input);
-  // var responses = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!"]
+function beepConverter(inputValue) {
+
   var result = "";
-  var sorry = input % 3 === 0
+  var sorry = inputValue % 3 === 0
 
-  if (input > 0 && sorry) {
-    // for (var i=0; i===input; i++) {
+  if (inputValue > 0 && sorry) {
     result = "I'm sorry, Dave. I'm afraid I can't do that.";
-    // }
-  } else if (input.includes('1')) {
+  } else if (inputValue.includes('1')) {
     result = "Boop!";
-  } else if (input.includes('0')) {
+  } else if (inputValue.includes('0')) {
     result = "Beep!";
-  } else result = input;
+  } else result = inputValue;
 
-console.log(sorry);
+  debugger;
 
   return result;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function() {
   $("#beepConverter").submit(function(event) {
   event.preventDefault();
 
+  var inputValue = $("#userInput").val();
+  var userInputData = parseInt($("#userInput").val());
+  var retort = beepConverter(inputValue);
+
+  
 
   });
 });
