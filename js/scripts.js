bc394dcc790788cffeb7e913fd0636ba6b987934
Matt Nardoni <nardoni59@gmail.com>
2018-03-30 12:02:02 -0700

    Add branching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 function beepConverter(input) {
+  // var inputNumber = parseInt(input);
+  // var responses = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!"]
   var result = "";
+  var sorry = input % 3 === 0
 
-  if ((input % 3) === 0){
+  if (input > 0 && sorry) {
+    // for (var i=0; i===input; i++) {
     result = "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else if (input === 1) {
+    // }
+  } else if (input.includes('1')) {
     result = "Boop!";
-  } else if (input === 0) {
+  } else if (input.includes('0')) {
     result = "Beep!";
-  }
+  } else result = input;
 
-  
+console.log(sorry);
+
   return result;
 }
 
