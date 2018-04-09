3677834c5283cb5a01c3ee7eb2b2d803243c43d9
Matt Nardoni <mattnardoni@MattNardoni.netgear.com>
2018-04-08 17:31:14 -0700

    Change backend logic to use an array with for loop and frontend logic to display one result

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,26 @@
 function beepConverter(inputValue) {
-
-  var result = "";
-  var sorry = inputValue % 3 === 0
-
-  if (inputValue > 0 && sorry) {
-    result = "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else if (inputValue.includes('1')) {
-    result = "Boop!";
-  } else if (inputValue.includes('0')) {
-    result = "Beep!";
-  } else result = inputValue;
-
+  var result = [];
+  for (var i = 0; i <= inputValue; i++) {
+    var numbers = i.toString().split("");
+    if (inputValue % 3 === 0) {
+      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (numbers.includes('1')) {
+      result.push("Boop!");
+    } else if (numbers.includes('0')) {
+      result.push("Beep!");
+    } else {
+    result.push(" " + i);
+    }
+  }
   return result;
 }
 
 $(document).ready(function() {
   $("#beepConverter").submit(function(event) {
-  var inputValue = $("#userInput").val();
-  var userInputData = parseInt($("#userInput").val());
-  var retort = beepConverter(inputValue);
-
-  if (retort === "I'm sorry, Dave. I'm afraid I can't do that.") {
-    $(".dave").toggle();
-  } else if (retort === "Boop!") {
-    $(".boop").toggle();
-  } else if (retort === "Beep!") {
-    $(".beep").toggle();
-  } else {
-    $(".number").show(inputValue);
-  }
-
     event.preventDefault();
+  var inputValue = $("#userInput").val();
+  $("#output").text(beepConverter(userInput));
+  $(".output").show();
 
   });
 });
